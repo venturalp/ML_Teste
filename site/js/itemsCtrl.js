3a6ee7cd235d2b06289840eb92e37813923be35c
Guilherme Ventura <venturalp@gmail.com>
2017-12-03 23:03:49 -0200

    ajustes de tela e responsivo

--- site/js/itemsCtrl.js
+++ site/js/itemsCtrl.js
@@ -1,4 +1,22 @@
-app.controller('itemCtrl', ['$scope', '$routeParams', '$location', '$rootScope', function ($scope, $routeParams, $location, $rootScope) {
-		alert('Sua busca: ' + $routeParams.id);
-		$rootScope.txtBusca = '';
-	}]);
+app.controller('itemCtrl', ['$scope', '$routeParams', '$location', '$rootScope', '$http', function ($scope, $routeParams, $location, $rootScope, $http) {
+//	alert('Sua busca: ' + $routeParams.id);
+	$rootScope.txtBusca = '';
+	$scope.carregando = false;
+	
+	$rootScope.buscar = function(){
+		window.location = "#!/items?search=" + $rootScope.txtBusca;
+	}
+		
+	$scope.getDados = function(){
+		$scope.carregando = true;		
+		$http.get('http://localhost:3000/api/items/' + $routeParams.id).then(function(res){
+			$scope.carregando = false;
+			$scope.item = res.data.item;			
+		}, function(err){
+			$scope.carregando = false;
+			console.log(err);
+		})
+	}
+	
+	$scope.getDados();
+}]);
